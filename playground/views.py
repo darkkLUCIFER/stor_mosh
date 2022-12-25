ca19336d6d080f327f174f749e38eca103f2dfc8
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-12-25 21:22:49 +0330

    test of content type

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.db.models import Count, Min, Max
 from django.db.models import Value, F
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Order
+from tags.models import TaggedItem
 
 
 def hello(request):
-    query_set = Product.objects.annotate(is_new=F('id'))
-    return render(request, 'playground/hello.html', {'products': query_set})
+    content_type = ContentType.objects.get_for_model(Product)
+
+    query_set = TaggedItem.objects.select_related('tag').filter(
+        content_type=content_type,
+        object_id=1
+    )
+    return render(request, 'playground/hello.html', {'tags': list(query_set)})
