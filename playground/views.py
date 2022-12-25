2dcdcce42362e32cbc2b8aff6a31d5ab3d5f6cd3
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-12-25 21:40:05 +0330

    added custom manager for TaggedItem model and used that

--- playground/views.py
+++ playground/views.py
@@ -7,10 +7,5 @@ from tags.models import TaggedItem
 
 
 def hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
-
-    query_set = TaggedItem.objects.select_related('tag').filter(
-        content_type=content_type,
-        object_id=1
-    )
-    return render(request, 'playground/hello.html', {'tags': list(query_set)})
+    tags = TaggedItem.objects.get_tags_for(Product, 1)
+    return render(request, 'playground/hello.html', {'tags': list(tags)})
