3f994aa6a531b18fc35712c20c5240ac785bb19c
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-12-27 17:19:35 +0330

    added custom cumputed column to Product admin

--- store/admin.py
+++ store/admin.py
@@ -3,4 +3,20 @@ from store.models import Customer, Collection, Product
 
 admin.site.register(Collection)
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
