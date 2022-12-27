b3e3ae4cd0563404c7cf90e7ba9af13cf8c7d751
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-12-27 12:45:04 +0330

    test transaction.atomic

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.db.models import Count, Min, Max
 from django.db.models import Value, F
+from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product, Order
+from store.models import Product, Order, Collection, OrderItem
 from tags.models import TaggedItem
 
 
+# @transaction.atomic()
 def hello(request):
-    tags = TaggedItem.objects.get_tags_for(Product, 1)
-    return render(request, 'playground/hello.html', {'tags': list(tags)})
+    with transaction.atomic():
+        order = Order.objects.create(customer_id=1)
+
+        item = OrderItem.objects.create(order=order, product_id=1, quantity=1, unit_price=10)
+    return render(request, 'playground/hello.html', {})
